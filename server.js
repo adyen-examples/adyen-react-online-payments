8b27d76d0c261b31ba3201fe131054c5ec5465bc
deepu105 <d4udts@gmail.com>
2020-03-20 13:19:38 +0100

    add dependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
