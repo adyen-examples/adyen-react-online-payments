4d12bffb308a5d75c77f747a4663f503130e2708
deepu105 <d4udts@gmail.com>
2020-03-20 13:22:00 +0100

    add .env file

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// enables environment variables by
+// parsing the .env file and assigning it to process.env
+dotenv.config({
+  path: "./.env"
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
